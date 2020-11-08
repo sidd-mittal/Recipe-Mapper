9cbfed76e2050a94af9c5b936f6722efd539865f
Jerry Tian <57043707+qt22@users.noreply.github.com>
2020-11-08 01:39:18 -0500

    Update router.js

--- api/router.js
+++ api/router.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index.ejs', { name: 'Jerry'})
+    res.render('index.ejs', { name: 'HackAttack Team 6'})
 })
 
 router.get('/login', (req, res) => {
     res.render('login.ejs')
+})
+
+router.post('/login', (req, res) => {
+    res.redirect('https://www.figma.com/proto/MrIZ3lzQmVt4snuYVqJ2wT/Recipe-Mapper-(SYDE%2FBME-Hackathon)?node-id=5%3A0&scaling=min-zoom')
 })
 
 router.get('/register', (req, res) => {
@@ -14,7 +18,7 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-    
+    res.redirect('/login')
 })
 
 module.exports = router;
